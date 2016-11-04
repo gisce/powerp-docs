e872b1ab607f988bc0e70a14cd0ade32a2f2353f
Jaume Florez <jflorez@gisce.net>
2016-11-04 10:07:20 +0100

    Added sitecustomize

---
+++ sitecustomize.py
@@ -0,0 +1,10 @@
+import sys
+actual_encoding = sys.getdefaultencoding()
+try:
+    import locale
+    loc = locale.getdefaultlocale()
+    if loc[1]:
+        encoding = loc[1]
+        sys.setdefaultencoding(encoding)
+except ImportError:
+    pass
