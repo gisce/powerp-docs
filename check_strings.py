0ddec08be1a48ba037548d19e90dba23e73a7cec
Jaume Florez <jflorez@gisce.net>
2017-01-09 10:38:17 +0100

    Updated test. Had some improves and a bug fix.
    - Removed check to first poid ("") as it is needed to
     load the header but will not be ever translated.
    - Added colored output
    - Improved output messages

--- check_strings.py
+++ check_strings.py
@@ -1,7 +1,11 @@
-from __future__ import division
+from __future__ import division, print_function
 from os.path import abspath, normpath, dirname, join, isfile
 from babel.messages import pofile
 import os
+
+green = "\033[0;32m"
+red = "\033[0;31m"
+endcl = "\033[0m"
 
 cwd = os.getcwd()
 print (u'Running on : {}'.format(cwd))
@@ -20,36 +24,49 @@ with open(es_path, 'r') as po:
     es_file = pofile.read_po(po)
 
 if not pot_file or not es_file:
-    print (u'messages.pot or messages.po could not be readed')
-
+    print (u'{}messages.pot or messages.po could not be readed'.format(
+        red
+    ))
 
 strings = [s.id for s in pot_file]
+strings.remove("")
 strings = set(strings)
 es_strings = []
 t_strings = []
 failed_strings = []
-print (u'Checking for all strings to be translated...')
+print (u'Checking for all strings to be translated...', end="")
 for s in es_file:
     es_strings.append(s.id)
-    if not(s.id and s.string) or s.fuzzy:
-        failed_strings.append(s.id)
+    if s.id == "":
+        es_strings.remove("")
+    elif not(s.id and s.string) or s.fuzzy:
+        failed_strings.append(s.id)        
+        print (u'{}There are strings not translated!{}'.format(
+            red, endcl
+        ))
     else:
         t_strings.append(s.string)
 
-print (u'Checking all strings in pot to be in po...')
+if not failed_strings:
+    print(u'{}OK{}'.format(green, endcl))
+
+print (u'Checking all strings in pot to be in po...', end="")
 es_strings = set(es_strings)
 substr = strings - es_strings
 if substr != set():
-    print (u'There are {0} missing strings in the po file!\n{1}'.format(
-        len(substr), list(substr)
+    print (u'{0}There are {1} missing strings in the po file!\n{2}'.format(
+        red, len(substr), list(substr)
     ))
     exit(-1)
-    
+print ('{}OK{}'.format(green, endcl))   
+
 print (u'\nTranslated strings: {0}/{1} ({2}%)\n'.format(
     len(t_strings), len(strings), len(t_strings)/len(strings) * 100
 ))
 if failed_strings:
-    print (u'There are strings not translated!\n{}'.format(failed_strings))
+    print (u'Missing strings:\n{}'.format(
+        failed_strings
+    ))
     exit(-1)
 
-print (u'Success!')
+print (u'{}Success!{}'.format(green, endcl))
