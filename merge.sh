6ddff96ba9a0d526671ed3573ae34c7a8f1f60b3
Jaume Florez <jflorez@gisce.net>
2017-02-03 11:20:58 +0100

    Updated merge script without "`" character

--- merge.sh
+++ merge.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "Copy local po file before merge"
-`cp locales/es_ES/LC_MESSAGES/messages.po messages.local.po`
+cp locales/es_ES/LC_MESSAGES/messages.po messages.local.po
 echo "Get remote po file from master before merge to avoid conflicts"
-`git checkout master -- locales/es_ES/LC_MESSAGES/messages.po`
-`cp locales/es_ES/LC_MESSAGES/messages.po messages.remote.po`
+git checkout master -- locales/es_ES/LC_MESSAGES/messages.po
+cp locales/es_ES/LC_MESSAGES/messages.po messages.remote.po
 echo "Merging from master"
 git merge origin/master
 echo "Merging pofile using msgcat"
-`msgcat --use-first messages.local.po messages.remote.po -o locales/es_ES/LC_MESSAGES/messages.po`
+msgcat --use-first messages.local.po messages.remote.po -o locales/es_ES/LC_MESSAGES/messages.po
 echo "Cleaning repository"
-`git add locales/es_ES/LC_MESSAGES/messages.po`
-`rm messages.local.po`
-`rm messages.remote.po`
+git add locales/es_ES/LC_MESSAGES/messages.po
+rm messages.local.po
+rm messages.remote.po
 echo "DONE - use 'git commit' to end merging"
 
