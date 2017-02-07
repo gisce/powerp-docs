977f98a33d5e6a521a64e733b4257c5d8812cd90
Francesc Puig <fpuig@gisce.net>
2017-02-07 10:11:54 +0100

    Add check before delete pot file

--- translate.sh
+++ translate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+if [ -f locales/messages.pot ];
+then
 echo "Erasing old pot file (local cache)..."
-`rm locales/messages.pot`
+rm locales/messages.pot
+fi
 echo "Building docs to get new pot file..."
-`mkdocs build -f mkdocs_es.yml --clean`
+mkdocs build -f mkdocs_es.yml --clean
 echo "Merging pofiles..."
-`msgmerge -U locales/es_ES/LC_MESSAGES/messages.po locales/messages.pot`
+msgmerge -U locales/es_ES/LC_MESSAGES/messages.po locales/messages.pot
 echo "Opening updated pofile with poedit..."
-`poedit locales/es_ES/LC_MESSAGES/messages.po`
+poedit locales/es_ES/LC_MESSAGES/messages.po
 python check_strings.py
 
